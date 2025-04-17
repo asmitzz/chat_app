28297c243b071d743f3c78fe78cf5120747adc06
Asmit Shrivastava <asmitzz@Mac.lan>
2025-04-17 13:55:53 +0530

    Added loggers for error

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -15,6 +15,15 @@ const logger = winston.createLogger({
       awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
       awsRegion: process.env.AWS_REGION,
     }),
+    new WinstonCloudWatch({
+        level: "error",
+        logGroupName: "ey-chat-logs",
+        logStreamName: "errors",
+        jsonMessage: true,
+        awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+        awsRegion: process.env.AWS_REGION,
+      }),
   ],
 });
 
